a8f8a1a443ff01d1a9cbba09d4d0762c2bf082c3
DIFIKYT <68289492+DIFIKYT@users.noreply.github.com>
2024-07-18 15:27:00 +0700

    Commit

--- Assets/Scripts/Abstract/SpawnerUI.cs
+++ Assets/Scripts/Abstract/SpawnerUI.cs
@@ -1,12 +1,12 @@
 using TMPro;
 using UnityEngine;
 
-public class SpawnerUI<T> : MonoBehaviour where T : Item
+public abstract class SpawnerUI<T> : MonoBehaviour where T : Item
 {
     [SerializeField] private TextMeshProUGUI _text;
     [SerializeField] private Spawner<T> _spawner;
 
-    private void Update()
+    protected virtual void Update()
     {
         _text.text = ($"{typeof(T).Name}s Created: {_spawner.TotalCreatedObjects} Active: {_spawner.AmountActiveObjects}");
     }
